9e4924b283d3eb7f998a9c076a740dd05514ce9b
CoderEslam <eslamghazy600@gmail.com>
2023-03-14 09:21:21 +0200

    Compose App

---
+++ app/src/main/java/com/doubleclick/learncompose/BoxActivity.kt
@@ -0,0 +1,58 @@
+package com.doubleclick.learncompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.doubleclick.learncompose.ui.theme.LearnComposeTheme
+
+class BoxActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LearnComposeTheme {
+                box()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun box() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .background(Color.Blue)
+                .width(100.dp)
+                .height(100.dp)
+                .verticalScroll(rememberScrollState(), true)
+                .horizontalScroll(rememberScrollState()),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(50.dp)
+                    .width(50.dp)
+                    .background(Color.White)
+            )
+            Text(text = "Hello \nEslam", fontSize = 50.sp)
+        }
+    }
+}
